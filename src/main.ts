c8661709cbcc284624cf6465b60ff08d927e26d5
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-06-22 01:04:01 +0300

    chore: test precommit

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-if(environment.production) {
-  enableProdMode();
+if (environment.production) {
+enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
