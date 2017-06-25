4d12b5364c5376e8b1cc91453055c03aa3a3bec1
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-06-25 15:56:21 +0300

    feat: добавил диалог добавления

--- src/app/containers/my-dairy-page.ts
+++ src/app/containers/my-dairy-page.ts
@@ -8,13 +8,22 @@ import * as diaryEntries from '../actions/diary-entries';
 
 import {DiaryEntry} from '../models/diary-entry';
 
+import {MdDialog, MdDialogRef} from '@angular/material';
+
+import {EntryCreateDialogComponent} from './create-entry-dialog';
+
 @Component({
   selector: 'diary-page',
   template: `
     <common-show-if-logged-in>
       <div>
+        <button md-button (click)="openDialog()">+ Добавить запись</button>
+      </div>
+      <!--
+      <div>
         <diary-entry-create></diary-entry-create>
       </div>
+      -->
       <div>
         <diary-entry-list [entries]="diaryEntries$ | async"></diary-entry-list>
       </div>
@@ -22,9 +31,10 @@ import {DiaryEntry} from '../models/diary-entry';
   `
 })
 export class MyDairyPageComponent {
+  selectedOption: any;
   diaryEntries$: Observable<DiaryEntry[]>;
 
-  constructor(private store: Store<fromRoot.State>) {
+  constructor(private store: Store<fromRoot.State>, public dialog: MdDialog) {
     this.diaryEntries$ = store.select(fromRoot.getEntries);
     store.select(fromRoot.getIsLoggedIn)
       .subscribe((isLoggedIn) => {
@@ -32,5 +42,13 @@ export class MyDairyPageComponent {
           this.store.dispatch(new diaryEntries.LoadListAction());
         }
       });
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(EntryCreateDialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      this.selectedOption = result;
+    });
+    // this.store.dispatch(new diaryDialogs.OpenCreateDialogAction());
   }
 }
