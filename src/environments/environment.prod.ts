236ecd2668ca7068e30e575cfbea6a5e7b2cd81e
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-06-27 23:03:41 +0300

    fix: исправлена часть багов

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,10 @@ import firebaseConfig from './firebase';
 export const environment = {
   production: true,
   firebase: {
-    ...firebaseConfig,
-    authDomain: 'diary-app-1fa1b.firebaseapp.com'
+    apiKey: 'AIzaSyCJcbeUFfA0FMq7nTR6uF0vGZvcle0Um3g',
+    authDomain: 'diary-app-1fa1b.firebaseapp.com',
+    databaseURL: 'https://diary-app-1fa1b.firebaseio.com/',
+    projectId: 'diary-app-1fa1b',
+    storageBucket: 'gs://diary-app-1fa1b.appspot.com/'
   }
 };
