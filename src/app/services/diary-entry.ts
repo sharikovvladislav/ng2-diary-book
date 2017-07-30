dfb7b694323a28eff3bf2c8f64b3b612b640b61b
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-07-30 17:42:52 +0300

    refactor: change way of getting user diaries

--- src/app/services/diary-entry.ts
+++ src/app/services/diary-entry.ts
@@ -6,24 +6,28 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 import { DiaryEntry } from '../models/diary-entry';
 import { DiaryEntrySet } from '../models/diary-entry-set';
 import { DiaryProcessorService } from './diary-processor';
+import { HttpClient } from '@angular/common/http';
+import { RemoteList } from '../models/remote-list';
+import { environment } from '../../environments/environment';
 
 
 @Injectable()
 export class DiaryEntryService {
   private API_PATH = '/diaryEntries';
   private list$: FirebaseListObservable<{}>;
+  private API_REST_URL = environment.firebase.restURL;
 
   constructor(
     private db: AngularFireDatabase,
-    private diaryProcessor: DiaryProcessorService
+    private diaryProcessor: DiaryProcessorService,
+    private http: HttpClient,
   ) {
     this.list$ = this.db.list(this.API_PATH);
   }
 
   retrieveEntries(uid: string): Observable<DiaryEntry[]> {
-    return this.getDbRef(uid)
-      .map((res) => (res || []).reverse())
-      .take(1);
+    return this.http.get(`${this.API_REST_URL}diaryEntries`) ///app/diaryEntries
+      .map((diaryEntries: RemoteList) => diaryEntries.items);
   }
 
   createEntry(uid: string, entryData: DiaryEntrySet): Observable<DiaryEntry> {
