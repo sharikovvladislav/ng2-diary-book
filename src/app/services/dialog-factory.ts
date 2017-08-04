ae95b4b052501d7166b7036f567604d54ae26218
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-04 23:44:45 +0300

    bugfix(dialogs): edit & create dialogs was not closing

---
+++ src/app/services/dialog-factory.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { EntryCreateDialogComponent } from '../containers/create-entry-dialog';
+import { MdDialog } from '@angular/material';
+import { EntryEditDialogComponent } from '../containers/edit-entry-dialog';
+
+@Injectable()
+export class DialogFactoryService {
+  editEntryDialog: any = null;
+  createEntryDialog: any = null;
+
+  constructor(private dialog: MdDialog) {
+  }
+
+  openCreateEntryDialog(): void {
+    this.editEntryDialog = this.dialog.open(EntryCreateDialogComponent, {
+      disableClose: true
+    });
+  }
+
+  closeCreateEntryDialog(): void {
+    this.editEntryDialog.close();
+  }
+
+  openEditEntryDialog(dialogData: any): void {
+    this.createEntryDialog = this.dialog.open(EntryEditDialogComponent, {
+      data: dialogData,
+      disableClose: true
+    });
+  }
+
+  closeEditEntryDialog(): void {
+    this.createEntryDialog.close();
+  }
+}
