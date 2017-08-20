bb905b3b595badddf1fec490be326d28b80c537e
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-21 01:11:34 +0300

    chore: prepare repro

--- src/app/shared/pipes/markdown.ts
+++ src/app/shared/pipes/markdown.ts
@@ -5,8 +5,8 @@ import markdown from 'markdown-js';
 @Pipe({ name: 'diaryMarkdown' })
 export class MarkdownPipe implements PipeTransform {
   transform(textAsMarkdown: string) {
-    // FIXME try to get why markdown is undefined in test environment
-    return textAsMarkdown;
-    //  return markdown.makeHtml(textAsMarkdown);
+    // TODO here is the problem, in test environment markdown is undefined
+    console.log('markdown in MarkdownPipe', markdown);
+    return markdown.makeHtml(textAsMarkdown);
   }
 }
