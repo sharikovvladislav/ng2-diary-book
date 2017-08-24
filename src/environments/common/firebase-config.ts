1a9ab6ca49f9c9de4ec588da130c8569ea161b1b
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-24 23:05:17 +0300

    refactor: eject firebase configuration to separate file

---
+++ src/environments/common/firebase-config.ts
@@ -0,0 +1,10 @@
+const firebase = {
+  apiKey: 'AIzaSyCJcbeUFfA0FMq7nTR6uF0vGZvcle0Um3g',
+  authDomain: 'diary-app-1fa1b.firebaseapp.com',
+  restURL: 'https://us-central1-diary-app-1fa1b.cloudfunctions.net/app/',
+  databaseURL: 'https://diary-app-1fa1b.firebaseio.com/',
+  projectId: 'diary-app-1fa1b',
+  storageBucket: 'gs://diary-app-1fa1b.appspot.com/',
+};
+
+export { firebase };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,6 @@
+import { firebase } from './common/firebase-config';
+
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCJcbeUFfA0FMq7nTR6uF0vGZvcle0Um3g',
-    authDomain: 'diary-app-1fa1b.firebaseapp.com',
-    databaseURL: 'https://diary-app-1fa1b.firebaseio.com/',
-    restURL: 'https://us-central1-diary-app-1fa1b.cloudfunctions.net/app/',
-    projectId: 'diary-app-1fa1b',
-    storageBucket: 'gs://diary-app-1fa1b.appspot.com/',
-  },
+  firebase,
 };
