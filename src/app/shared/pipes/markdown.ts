3ef43cf94b0df3c217ae69233826a225345ea711
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-10-04 08:42:36 +0300

    refactor: fix adding items

--- src/app/shared/pipes/markdown.ts
+++ src/app/shared/pipes/markdown.ts
@@ -4,7 +4,7 @@ import * as markdown from 'markdown-js';
 
 @Pipe({ name: 'diaryMarkdown' })
 export class MarkdownPipe implements PipeTransform {
-  transform(textAsMarkdown: string) {
+  transform(textAsMarkdown: string = '') {
     return markdown.makeHtml(textAsMarkdown);
   }
 }
