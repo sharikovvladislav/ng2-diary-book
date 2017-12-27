ac29dd5de22b0698c30ac4016d62fd97f5faed07
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-12-28 00:39:54 +0300

    test: create my-test-bad class helper and add shallow renderer there

--- src/app/core/components/user-info.spec.ts
+++ src/app/core/components/user-info.spec.ts
@@ -4,72 +4,95 @@ import { UserInfoComponent } from './user-info';
 import { MdIconModule } from '@angular/material';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { MyTestBed } from '../../test/helpers/my-test-bed';
 
 describe('UserInfoComponent', () => {
-  let component: UserInfoComponent;
-  let fixture: ComponentFixture<UserInfoComponent>;
+  describe('shallow tests', () => {
+    let component: UserInfoComponent;
+    let fixture: ComponentFixture<UserInfoComponent>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        imports: [MdIconModule],
-        declarations: [UserInfoComponent],
-      }).compileComponents();
-    }),
-  );
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserInfoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  describe('should handle photoURL correctly', () => {
-    describe('photoURL is passed', () => {
-      beforeEach(() => {
-        component.photoURL = 'somePhoto';
-        fixture.detectChanges();
-      });
-      it('hasPhotoURL should be truthy', () =>
-        expect(component.hasPhotoURL()).toBeTruthy());
-      it('should match snapshot', () =>
-        expect(fixture.nativeElement.innerHTML).toMatchSnapshot());
-    });
-    describe('photoURL is not passed', () => {
-      beforeEach(() => {
-        component.photoURL = '';
-        fixture.detectChanges();
-      });
-      it('hasPhotoURL should be falsy', () =>
-        expect(component.hasPhotoURL()).toBeFalsy());
-      it('should match snapshot', () =>
-        expect(fixture.nativeElement.innerHTML).toMatchSnapshot());
-    });
-  });
-
-  describe('should handle displayName correctly', () => {
-    it('should show display name if it is passed', () => {
-      component.displayName = 'vova chuma';
-      fixture.detectChanges();
-
-      expect(fixture.nativeElement.innerHTML).toMatchSnapshot();
-    });
-  });
-
-  it('should handle click on exit button', () => {
-    const spy = jest.fn();
-    component.onExit.subscribe(spy);
-
-    const de: DebugElement = fixture.debugElement.query(
-      By.css('span > md-icon'),
+    beforeEach(
+      async(() => {
+        MyTestBed.configureTestingModule({
+          imports: [MdIconModule],
+          declarations: [UserInfoComponent],
+        })
+          .shallowRenderComponent(UserInfoComponent)
+          .compileComponents();
+      }),
     );
 
-    de.triggerEventHandler('click', null);
+    beforeEach(() => {
+      fixture = TestBed.createComponent(UserInfoComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-    expect(spy).toHaveBeenCalled();
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    describe('should handle photoURL correctly', () => {
+      describe('photoURL is passed', () => {
+        beforeEach(() => {
+          component.photoURL = 'somePhoto';
+          fixture.detectChanges();
+        });
+        it('hasPhotoURL should be truthy', () =>
+          expect(component.hasPhotoURL()).toBeTruthy());
+        it('should match snapshot', () => expect(fixture).toMatchSnapshot());
+      });
+      describe('photoURL is not passed', () => {
+        beforeEach(() => {
+          component.photoURL = '';
+          fixture.detectChanges();
+        });
+        it('hasPhotoURL should be falsy', () =>
+          expect(component.hasPhotoURL()).toBeFalsy());
+        it('should match snapshot', () => expect(fixture).toMatchSnapshot());
+      });
+    });
+
+    describe('should handle displayName correctly', () => {
+      it('should show display name if it is passed', () => {
+        component.displayName = 'vova chuma';
+        fixture.detectChanges();
+
+        expect(fixture).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('not shallow tests', () => {
+    let component: UserInfoComponent;
+    let fixture: ComponentFixture<UserInfoComponent>;
+
+    beforeEach(
+      async(() => {
+        MyTestBed.configureTestingModule({
+          imports: [MdIconModule],
+          declarations: [UserInfoComponent],
+        }).compileComponents();
+      }),
+    );
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(UserInfoComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should handle click on exit button', () => {
+      const spy = jest.fn();
+      component.onExit.subscribe(spy);
+
+      const de: DebugElement = fixture.debugElement.query(
+        By.css('span > md-icon'),
+      );
+
+      de.triggerEventHandler('click', null);
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
